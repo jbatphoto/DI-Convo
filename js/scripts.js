394f71f289b9565cf4d72b7a65d5d5127f8ddcff
Justin Battenfield <justinbattenfield@Dentals-MacBook-Pro.local>
2020-11-20 09:49:52 -0600

    test

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,16 @@ $(document).ready(function(){
       $("#closed").on("click", function(){
         $("#result").load("partials/closed.html");
       });  
-      $("#jacob").on("click", function(){
-        $("#message-result").load("partials/jacob.html");
+});
+
+$(document).ready(function(){ 
+    $("#jacob").on("click", function(){
+        $("#message-result").load("messages/jacob.html");
       }); 
       $("#pete").on("click", function(){
         $("#message-result").load("messages/pete.html");
       }); 
       $("#weston").on("click", function(){
         $("#message-result").load("messages/weston.html");
-      }); 
-});
-
-$(document).ready(function(){ 
-    $("#message-result").load("messages/pete.html");
-     
+      });  
 });
